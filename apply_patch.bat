aafaea1055c0c9e1251903f75f51581b707101a1
Grego Lewandowski <original_grego@yahoo.com>
2022-03-21 10:54:54 -0400

    Whoops

--- apply_patch.bat
+++ apply_patch.bat
@@ -7,9 +7,6 @@ Asm68k.exe /p /o ae+ /o c+ /o l+ src\zerowing_hack.asm, build\zerowing_hack.bin
 
 java -jar RomMangler.jar split split_cfgs\zerowing_out_split.cfg build\zerowing_hack.bin
 
-ROM_LOAD16_BYTE,build\out\o15-11ii.bin,000000,08000
-ROM_LOAD16_BYTE,build\out\o15-12ii.bin,000001,08000
-
 copy build\out\o15-11ii.bin %ROM_DIR%\zerowing\o15-11ii.bin
 copy build\out\o15-12ii.bin %ROM_DIR%\zerowing\o15-12ii.bin
 
